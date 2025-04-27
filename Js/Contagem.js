22030c1e22a9d7a767a3e813c324412d3737a67a
Guilherme Oliveira <guilherme_oliveira18@estudante.sesisenai.org.br>
2025-04-27 20:11:25 -0300

    contagem mais rapido

--- Js/Contagem.js
+++ Js/Contagem.js
@@ -1,14 +1,23 @@
 function animateCounter(element) {
     const targetValue = parseInt(element.getAttribute('data-to-value'));
-    let currentValue = 1;
+    let currentValue = 0;
+    const duration = 1000; // duração da animação em milissegundos (1 segundo)
+    const frameRate = 60; // 60 frames por segundo
+    const totalFrames = Math.round(duration / (1000 / frameRate));
+    const increment = targetValue / totalFrames;
 
-    const interval = setInterval(() => {
-        element.textContent = currentValue;
-        currentValue++;
-        if (currentValue > targetValue) {
-            clearInterval(interval);
+    let frame = 0;
+
+    const counterInterval = setInterval(() => {
+        frame++;
+        currentValue += increment;
+        if (frame >= totalFrames) {
+            clearInterval(counterInterval);
+            element.textContent = targetValue; // Garante o número exato no final
+        } else {
+            element.textContent = Math.round(currentValue);
         }
-    }, 0); // Atualiza a cada 10ms
+    }, 1000 / frameRate);
 }
 
 // Aplica a animação para todos os elementos com a classe 'counter-number'
